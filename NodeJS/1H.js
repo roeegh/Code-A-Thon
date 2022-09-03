7f450bd3853874a0e64914d6529b3cf1c3ad2712
Roeegh <admin@roeegh.dev>
2022-09-03 16:10:17 -0500

    updated comment

--- NodeJS/1H.js
+++ NodeJS/1H.js
@@ -1,6 +1,7 @@
 /* 
     Input: The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters.
     Output: Return the words of the initial song that Big Mike used to make a dubsteb remix. Separate the words with a space.
+
     Example songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB') => 'WE ARE THE CHAMPIONS MY FRIEND'
 */ 
 
@@ -8,5 +9,4 @@ const songDecoder = (remix) => {
     return remix.replace(/(WUB)+/g, ' ').trim();
 }
 
-console.log('Remix: WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB');
-console.log(`Original: ${songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB')}`);
+console.log(songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'));
